865e6716f4034108c5a1e14c0a03d2e925cc4aee
ramzy1453 <kemmounramzy93@gmail.com>
2024-12-12 19:02:17 +0100

    remove non used variable to prevent build errors

--- components/ui/CustomDialogHeader.tsx
+++ components/ui/CustomDialogHeader.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Icon, LucideIcon } from "lucide-react";
+import { LucideIcon } from "lucide-react";
 import { DialogHeader, DialogTitle } from "./dialog";
 import { cn } from "@/lib/utils";
 import { Separator } from "@radix-ui/react-dropdown-menu";
@@ -7,44 +7,48 @@ import { Separator } from "@radix-ui/react-dropdown-menu";
 interface Props {
   title?: string;
   subtitle?: string;
-  icon?: LucideIcon; 
+  icon?: LucideIcon;
   iconclassname?: string;
   titleclassname?: string;
   subtitleclassname?: string;
 }
 
 export function CustomDialogHeader(props: Props) {
-    const { icon: IconComponent, iconclassname } = props;
-  
-    return (
-      <DialogHeader className="py-6">
-        <DialogTitle asChild>
-          <div className="flex flex-col items-center gap-2 mb-2">
-            {IconComponent && (
-              <IconComponent
-                size={30}
-                className={cn("stroke-primary", iconclassname)}
-              />
-            )}
-            {props.title && (
-              <h3
-                className={cn(
-                  "text-lg font-semibold leading-none tracking-tight",
-                  props.titleclassname
-                )}
-              >
-                {props.title}
-              </h3>
-            )}
-            {props.subtitle && (
-              <p className={cn("text-sm text-muted-foreground", props.subtitleclassname)}>
-                {props.subtitle}
-              </p>
-            )}
-            <Separator />
-          </div>
-        </DialogTitle>
-      </DialogHeader>
-    );
-  }
-  
+  const { icon: IconComponent, iconclassname } = props;
+
+  return (
+    <DialogHeader className="py-6">
+      <DialogTitle asChild>
+        <div className="flex flex-col items-center gap-2 mb-2">
+          {IconComponent && (
+            <IconComponent
+              size={30}
+              className={cn("stroke-primary", iconclassname)}
+            />
+          )}
+          {props.title && (
+            <h3
+              className={cn(
+                "text-lg font-semibold leading-none tracking-tight",
+                props.titleclassname
+              )}
+            >
+              {props.title}
+            </h3>
+          )}
+          {props.subtitle && (
+            <p
+              className={cn(
+                "text-sm text-muted-foreground",
+                props.subtitleclassname
+              )}
+            >
+              {props.subtitle}
+            </p>
+          )}
+          <Separator />
+        </div>
+      </DialogTitle>
+    </DialogHeader>
+  );
+}
